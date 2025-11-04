3c307384657a573a34a59516f7a24c0556d3df09
usershokhrukh <asxurovsit@gmail.com>
2025-11-05 02:16:15 +0500

    modified: hello world

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace System
+{
+  class Project
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Hello world");
+    }
+  }
+}
