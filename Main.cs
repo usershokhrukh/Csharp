5239ddab1cf99e5670f87f71b0f2e5d8a513070f
usershokhrukh <asxurovsit@gmail.com>
2025-11-05 18:10:10 +0500

    basics up to date new project with C# basics

--- Main.cs
+++ Main.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Net.WebSockets;
-
-namespace System
-{
-  class Project
+namespace MyProject;
+class Program {
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
-    {
-      System.Console.WriteLine("Hello world");
-    }
-  }
+    // malumot olish
+    System.Console.Write("Komunnal xarajatni kiriting: ");
+    string? comunnalCost = Console.ReadLine();
+    
+    System.Console.Write("Oziq ovqat uchun harajat: ");
+    string? foodCost = Console.ReadLine();
+
+    System.Console.Write("Qora kun uchun: ");
+    string? unexpectedCost = Console.ReadLine();
+
+    // algorithm
+    double? communalCostPrice = double.Parse(comunnalCost);
+    double? foodCostPrice = double.Parse(foodCost);
+    double? unexpectedCostPrice = double.Parse(unexpectedCost);
+    double? totalCOst = communalCostPrice + foodCostPrice + unexpectedCostPrice;
+    double? smartRightCost = totalCOst * 0.15;
+
+    //showing result
+    System.Console.WriteLine("--- Oylik pul yoqolishi ---");
+    System.Console.WriteLine($"Kommunal xarajatlar  \t|\t{communalCostPrice}");
+    System.Console.WriteLine($"Oziq ovqat xarajatlar\t|\t{foodCostPrice}");
+    System.Console.WriteLine($"Qora kun             \t|\t{unexpectedCostPrice}");
+    System.Console.WriteLine($"hammasi bo'lib       \t|\t{totalCOst}");
+    System.Console.WriteLine($"Investitsiya         \t|\t{smartRightCost}");
+
+
+
+
+  } 
 }
